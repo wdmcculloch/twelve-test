df22e3d6ed5d8a5054597a070f1a21e5cc71b6a7
Will McCulloch <willdmcculloch@gmail.com>
2021-10-11 22:30:02 -0700

    uncomment express.static

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ const {
 
 app.use(cors());
 
-// app.use(express.static(path.join(__dirname, "../client/dist")));
+app.use(express.static(path.join(__dirname, "../client/dist")));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
