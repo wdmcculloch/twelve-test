7d4fa077d86caed0fb8a5cdde4148a8992390b6f
Will McCulloch <willdmcculloch@gmail.com>
2021-11-10 13:47:51 -0800

    update port to let heroku dynamically set

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 const cors = require('cors');
 const path = require("path");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const client = require("../db/index.js");
 const {
   getAllSectorNames,
